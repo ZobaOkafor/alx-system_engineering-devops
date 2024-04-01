48aa1d05a26ba6ce8ad9cf4550185b9e02410cb3
ZobaOkafor <judithokafor30@gmail.com>
2024-04-01 11:07:17 -0700

    Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,24 @@
 # Install Nginx package
 
+exec { 'apt-get update':
+  command  => 'apt-get update',
+  provider => shell,
+}
+
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['apt-get update'],
 }
 
-# Define custom HTTP header value
-$hostname = $facts['hostname']
-
-# Configure Nginx to add custom header
-file { '/etc/nginx/conf.d/custom_response_header.conf':
-  ensure  => present,
-  content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        proxy_pass http://localhost:8080;
-        add_header X-Served-By ${hostname};
-        proxy_set_header Host $http_host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-}",
-  notify  => Service['nginx'],
+file_line { 'Header response':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
-# Restart Nginx to apply changes
 service { 'nginx':
   ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/conf.d/custom_response_header.conf'],
+  require => Package['nginx'],
 }
